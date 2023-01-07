bd13c1956e28961e701731b274b98179964ab719
Corey <corey@confusingbits.com>
2023-01-07 13:17:30 -0600

    tutorial push

---
+++ Application/Fixtures.sql
@@ -0,0 +1,24 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.posts DISABLE TRIGGER ALL;
+
+INSERT INTO public.posts (id, title, body) VALUES ('4e3525b2-85ee-4f27-ab20-6d7fc021799e', 'test', 'body is here');
+
+
+ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
